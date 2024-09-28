aa6f1f39b9325b77967bf2c46f217e3b58208bd5
Hunold <raphael.hunold@gmail.com>
2024-09-28 13:33:37 +0200

    Connect to API LoginView, GetUserView and GetUserLikesView

--- ubiquote/persons/users/views.py
+++ ubiquote/persons/users/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+from django.contrib.auth import login
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.http import JsonResponse
+
 from django.urls import reverse, reverse_lazy
 from django.db.models import Max
 
@@ -23,11 +27,12 @@ from .models import User
 from .forms import  UserForm
 from django.conf import settings
 
-from texts.quotes.views import like_quote
+# from texts.quotes.views import like_quote
 
 from django.contrib.auth import views as auth_views
 from django.utils.translation import get_language
 
+import requests
 
 
 
@@ -37,108 +42,68 @@ class GetUserLikesView(LoginRequiredMixin, ListView):
     context_object_name = 'quotes'  
     template_name = 'get_user_likes.html'
     paginate_by = settings.DEFAULT_PAGINATION  # Number of items per page  
-    # login_url = reverse_lazy('login')  # URL name of your login page    
     
-    # def get(self, request, *args, **kwargs):
-    #     return super().get(request, *args, **kwargs)    
-  
+    # API URL for quotes list
+    api_url = 'http://127.0.0.1:8000/api'        
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        
-        # Get the profil slug from the URL parameter
-        profil_slug = self.kwargs['slug']
-        
-        context['profil_slug'] = profil_slug        
-        # Get the profil object based on the slug
-        profil = User.objects.get(slug=profil_slug)       
-        context['profil'] = profil
-        
-        # user = self.request.user      
-
-        quotes = context['quotes']  # Get the queryset of quotes
-        
-        # Have a track of liked quote to display like button
-        quotes_like_statut = {quote.id: QuotesLikes.has_user_liked(profil, quote) for quote in quotes}
-        liked_quotes = [quote_id for quote_id, liked in quotes_like_statut.items() if liked]  
-        context['liked_quotes'] = liked_quotes
-        
-        # get the timestamp of when the quoteslikes had been created
-        liked_quotes_timestamps = QuotesLikes.objects.filter(user=profil).values('quote_id', 'timestamp')
-        context['liked_quotes_timestamps'] = liked_quotes_timestamps
-
-
-        # Get total number of quotes in the database
-        total_quotes = Quote.published.filter(likes=profil).count()
-        context['total_quotes'] = total_quotes   
-        
-        user_language = get_language()   
-        
-        translated_names = {}
-        for quote in quotes:
-            author = quote.author
-            if author:
-                translated_names[quote.id] = author.get_translation(user_language)
-                #   print("1")
-            #   else:
-            #       translated_names[quote.id] = 'Unknown'
-            #     #   print('2')
-        
-        context['translated_names'] = translated_names         
-        
-        paginator = Paginator(self.get_queryset(), self.paginate_by)
-        page_number = self.request.GET.get('page')
-        page_obj = paginator.get_page(page_number)
-        context['page_obj'] = page_obj
-        context['paginator'] = paginator
-        
-    
-        # if page_obj.has_next():
-        #     user_slug = profil.slug # Assuming you're passing the profil in the class
-        #     print(user_slug)
-        #     next_page_url = reverse('users:get-user-likes', kwargs={'slug': user_slug}) + f'?page={page_obj.next_page_number()}'
-        # context['next_page_url'] = next_page_url
-        
-        # print(next_page_url)
-        
-        
         return context
-    
 
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_authenticated:
+
+    def get(self, request, *args, **kwargs):
+        page_number = request.GET.get('page', 1)
         
-            # Get the IDs of the liked quotes
-            liked_quote_ids = QuotesLikes.objects.filter(user=user).values_list('quote_id', flat=True)
+        profil_slug = self.kwargs['slug']
+        user = request.user
 
-            # Retrieve the quotes based on their IDs and order them by the latest timestamp
-            queryset = Quote.objects.filter(id__in=liked_quote_ids).select_related('author').annotate(latest_like_timestamp=Max('quoteslikes__timestamp')).order_by('-latest_like_timestamp')
+        # Adjust API URL to pass pagination and search query
+        likes_api_url = f'{self.api_url}/likes/{profil_slug}/?page={page_number}'
+        # quote_response = requests.get(api_url, headers={'Authorization': f'Token {request.user.auth_token}'})
+        
+        # quote_api_url = f'http://127.0.0.1:8000/api/user/{profil.slug}/quotes/?page={page_number}'
+        likes_response = requests.get(likes_api_url)
 
-
-            return queryset        
+        if likes_response.status_code == 200:
+            data = likes_response.json()
+            # print(data)
+            quotes = data.get('results', [])
+            count = data.get('count')               
+            next_page_url = data.get('next')
         else:
-            # Redirect anonymous users to the login page
-            # return redirect('users:login')
-            raise PermissionDenied
+            quotes = []
+            next_page_url = None
 
 
-    def get_template_names(self):
-        if self.request.htmx:
-            return ['quotes_cards.html']
-        return ['get_user_likes.html']        
+        profil_api_url = f'{self.api_url}/user/{profil_slug}/'
+        profil_response = requests.get(profil_api_url)
+
+        if profil_response.status_code == 200:
+            profil = profil_response.json()
+            # user = data.get('results', [])
+            # next_page_url = data.get('next')
+        else:
+            profil = []
+            # next_page_url = None            
+
+
+        context = {
+            'profil': profil, 
+            'quotes': quotes,
+            'page_number': page_number,    
+            'count': count,                      
+            'next_page_url': next_page_url,
+            # 'search_query': search_query,                    
+        }
         
+        # If it's an HTMX request, return only the quotes part
+        if request.htmx:
+
+            return render(request, 'quotes_cards.html', context)        
+
+        return render(request, self.template_name, context)    
 
 
-
-    # def render_to_response(self, context, **response_kwargs):
-    #     if self.request.htmx:
-    #         # If request is htmx, return the corresponding template
-    #         template = self.get_template_names()
-    #         return super().render_to_response(context, template=template, **response_kwargs)
-    #     else:
-    #         # If regular request, return normal response
-    #         return super().render_to_response(context, **response_kwargs)
-  
 
 class UserRegisterView(generic.CreateView):
     form_class = UserCreationForm
@@ -149,25 +114,48 @@ class UserRegisterView(generic.CreateView):
 class LoginView(auth_views.LoginView):
     form_class = AuthenticationForm
     template_name = 'registration/login.html'
-    
+    success_url = reverse_lazy('texts:get-home')  # Redirect to home after login
 
     def form_valid(self, form):
-        # Call the parent form_valid method
-        response = super().form_valid(form)
+        # Log the user in
+        user = form.get_user()
+        login(self.request, user)
 
-        # Retrieve the quote ID from the session
-        quote_id = self.request.GET.get('quote_id')
+        # Generate JWT token
+        refresh = RefreshToken.for_user(user)
+        access_token = str(refresh.access_token)
+        refresh_token = str(refresh)
+
+        # Store tokens in the session (or return them in a response)
+        self.request.session['access_token'] = access_token
+        self.request.session['refresh_token'] = refresh_token
         
-        # If the quote ID exists, like the quote and redirect the user
-        if quote_id:
-            # Like the quote (add your logic here)
-            like_quote(self.request, quote_id)
+        # You can return a JSON response with tokens or redirect
+        if self.request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            return JsonResponse({
+                'access_token': access_token,
+                'refresh_token': refresh_token,
+            })
+
+        return redirect(self.success_url)    
+
+    # def form_valid(self, form):
+    #     # Call the parent form_valid method
+    #     response = super().form_valid(form)
+
+    #     # Retrieve the quote ID from the session
+    #     quote_id = self.request.GET.get('quote_id')
+        
+    #     # If the quote ID exists, like the quote and redirect the user
+    #     if quote_id:
+    #         # Like the quote (add your logic here)
+    #         like_quote(self.request, quote_id)
             
-            # Redirect the user back to the initial page
-            return redirect(reverse('users:get-user-likes', kwargs={'slug': self.request.user.slug}))
+    #         # Redirect the user back to the initial page
+    #         return redirect(reverse('users:get-user-likes', kwargs={'slug': self.request.user.slug}))
         
 
-        return response  # Return the original response if no quote ID is found    
+    #     return response  # Return the original response if no quote ID is found    
     
 class LogoutView(auth_views.LogoutView):
     form_class = AuthenticationForm
@@ -176,60 +164,77 @@ class LogoutView(auth_views.LogoutView):
 
 
 class GetUsersView(ListView):
-  model = User
-  template_name = 'get_users.html'
-  context_object_name = 'users'
-  ordering =['username']
+    model = User
+    template_name = 'get_users.html'
+    context_object_name = 'users'
+    ordering =['username']
 
 
-class GetUserView(ListView):
-  model = Quote 
-  context_object_name = 'quotes'  
-  template_name = 'get_user.html'
-  paginate_by = settings.DEFAULT_PAGINATION    
-  
-  def get_context_data(self, **kwargs):
-      context = super().get_context_data(**kwargs)
-      
-      # Get the author object based on the slug
-      profil = User.objects.get(slug=self.kwargs['slug'])
-      context['profil'] = profil  # Pass the category object to the template
-      # context['title'] = category.title   
-      
-      # user = self.request.user
-      quotes = context['quotes']  # Get the queryset of quotes
-      quotes_like_statut = {quote.id: QuotesLikes.has_user_liked(profil, quote) for quote in quotes}
-      liked_quotes = [quote_id for quote_id, liked in quotes_like_statut.items() if liked]  
-      context['liked_quotes'] = liked_quotes      
-                  
-      # Get total number of quotes in the database
-      total_quotes = Quote.published.filter(contributor__slug=profil) .count()
-      context['total_quotes'] = total_quotes
-      
-      user_language = get_language()   
+class GetUserView(LoginRequiredMixin, ListView):
+    model = Quote 
+    context_object_name = 'quotes'  
+    template_name = 'get_user.html'
+    paginate_by = settings.DEFAULT_PAGINATION   
     
-      translated_names = {}
-      for quote in quotes:
-          author = quote.author
-          if author:
-              translated_names[quote.id] = author.get_translation(user_language)
-            #   print("1")
-          else:
-              translated_names[quote.id] = 'Unknown'
-            #   print('2')
-      
-      context['translated_names'] = translated_names       
-         
-      return context
+    # API URL for quotes list
+    api_url = 'http://127.0.0.1:8000/api'     
 
-  def get_queryset(self):
-      # Get the profil slug
-      user_slug = self.kwargs['slug']
-      
-      # Filter quotes by the contributor slug
-      queryset = Quote.published.filter(contributor__slug=user_slug).order_by("-date_created")
-      
-      return queryset
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
+
+
+    def get(self, request, *args, **kwargs):
+        page_number = request.GET.get('page', 1)
+        
+        user_slug = self.kwargs['slug']
+        profil = User.objects.get(slug=user_slug)
+
+        # Adjust API URL to pass pagination and search query
+        quote_api_url = f'{self.api_url}/user/{profil.slug}/quotes/?page={page_number}'
+        # quote_response = requests.get(api_url, headers={'Authorization': f'Token {request.user.auth_token}'})
+        
+        # quote_api_url = f'http://127.0.0.1:8000/api/user/{profil.slug}/quotes/?page={page_number}'
+        quote_response = requests.get(quote_api_url)
+
+        if quote_response.status_code == 200:
+            data = quote_response.json()
+            # print(data)
+            quotes = data.get('results', [])
+            count = data.get('count')               
+            next_page_url = data.get('next')
+        else:
+            quotes = []
+            next_page_url = None
+
+        profil_api_url = f'{self.api_url}/user/{profil.slug}/'
+        profil_response = requests.get(profil_api_url)
+
+        if profil_response.status_code == 200:
+            profil = profil_response.json()
+            # user = data.get('results', [])
+            # next_page_url = data.get('next')
+        else:
+            profil = []
+            # next_page_url = None            
+
+
+        context = {
+            'profil': profil, 
+            'quotes': quotes,
+            'page_number': page_number,    
+            'count': count,                      
+            'next_page_url': next_page_url,
+            # 'search_query': search_query,                    
+        }
+        
+        # If it's an HTMX request, return only the quotes part
+        if request.htmx:
+
+            return render(request, 'quotes_cards.html', context)        
+
+        return render(request, self.template_name, context)    
+
 
 
 class AddUserView(CreateView):
