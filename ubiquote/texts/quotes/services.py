3b927cf388a98955113babce3587b7c39c1726a0
Hunold <raphael.hunold@gmail.com>
2024-09-28 13:41:39 +0200

    recommandation module

--- ubiquote/texts/quotes/services.py
+++ ubiquote/texts/quotes/services.py
@@ -4,51 +4,17 @@ from sklearn.metrics.pairwise import cosine_similarity
 from .models import Quote, QuotesLikes
 from persons.users.models import User
 
+from django.shortcuts import get_object_or_404
+
 import time
 from django.db.models import Count, F  
 from django.core.cache import cache  # Import Django cache module
 
-# from django_pgvector.expressions import VectorField as PGVectorField
+from django.core.paginator import Paginator
+
+import numpy as np
 
 
-# class RecommendationService:
-#     @staticmethod
-#     def recommend_quotes(user_id, num_recommendations=10):
-#         start_time = time.time()  # Record start time
-        
-#         num_liked_quotes = QuotesLikes.objects.filter(user_id=user_id).count()
-        
-#         if num_liked_quotes != 0:
-#             liked_quotes_texts = QuotesLikes.objects.filter(user_id=user_id).values_list('quote__text', flat=True)
-            
-#             # Get liked quotes TF-IDF vectors
-#             liked_quotes_vectors = Quote.objects.filter(text__in=liked_quotes_texts).annotate(
-#                 tfidf_vector=PGVectorField('tfidf_vector')
-#             ).values_list('tfidf_vector', flat=True)
-            
-#             # Compute similarity between liked quotes and all quotes
-#             # Use PGVectorField for all_quotes_tfidf
-#             all_quotes_tfidf = Quote.objects.exclude(text__in=liked_quotes_texts).annotate(
-#                 tfidf_vector=PGVectorField('tfidf_vector')
-#             ).values_list('tfidf_vector', flat=True)
-#             similarity_matrix = cosine_similarity(liked_quotes_vectors, all_quotes_tfidf)
-            
-#             # Get indices of top similar quotes
-#             top_indices = similarity_matrix.argsort(axis=1)[:, -num_recommendations:]
-            
-#             # Get top recommended quotes
-#             recommended_quotes_ids = Quote.objects.exclude(text__in=liked_quotes_texts).values_list('id', flat=True)[top_indices.ravel()]
-#             recommended_quotes = Quote.objects.filter(id__in=recommended_quotes_ids)
-#         else:
-#             # Generate recommendations from top liked quotes
-#             top_quotes = Quote.objects.annotate(num_likes=Count('quoteslikes')).order_by('-num_likes', 'id')[:num_recommendations]
-#             recommended_quotes = top_quotes
-        
-#         end_time = time.time()  # Record end time
-#         execution_time = end_time - start_time
-#         print(f"Execution time: {execution_time:.4f} seconds")  # Display execution time
-
-#         return recommended_quotes
 
 
 class RecommendationService:
@@ -60,52 +26,67 @@ class RecommendationService:
     def get_vectorizer():
         if RecommendationService.vectorizer is None:
             RecommendationService.vectorizer = TfidfVectorizer()
-        return RecommendationService.vectorizer
+        return RecommendationService.vectorizer 
     
     @staticmethod
     def recommend_quotes(user_id, num_recommendations=10):
-        
         start_time = time.time()  # Record start time
         
-        num_liked_quotes = QuotesLikes.objects.filter(user_id=user_id).count()
+        # Check if user_id is valid
+        if user_id is None:
+            return Quote.objects.annotate(num_likes=Count('quoteslikes')).order_by('-num_likes')[:num_recommendations]
+        
+        
+        # user = get_object_or_404(User, id=user_id)
+        
+        num_liked_quotes = QuotesLikes.objects.filter(user_id=user_id).count()  
         
         if num_liked_quotes != 0:
             liked_quotes_texts = QuotesLikes.objects.filter(user_id=user_id).values_list('quote__text', flat=True)
 
             # Compute TF-IDF vectors for liked quotes
             vectorizer = RecommendationService.get_vectorizer()
-            liked_quotes_tfidf = vectorizer.fit_transform(liked_quotes_texts)
+            
+            # Fit only if the user has new liked quotes (cache num_liked_quotes)
+            if RecommendationService.cached_num_liked_quotes != num_liked_quotes:
+                liked_quotes_tfidf = vectorizer.fit_transform(liked_quotes_texts)
+                RecommendationService.cached_num_liked_quotes = num_liked_quotes
+            else:
+                liked_quotes_tfidf = vectorizer.transform(liked_quotes_texts)            
             
             # Get all quotes texts excluding the ones liked by the user
             excluded_quotes_texts = QuotesLikes.objects.filter(user_id=user_id).values_list('quote__text', flat=True)
             all_quotes_texts = Quote.objects.exclude(text__in=excluded_quotes_texts).values_list('text', flat=True)
-            
 
             # Compute similarity between liked quotes and all quotes
-            # all_quotes_texts = Quote.objects.exclude(quoteslikes__user_id=user_id).values_list('text', flat=True)
             all_quotes_tfidf = vectorizer.transform(all_quotes_texts)
             similarity_matrix = cosine_similarity(liked_quotes_tfidf, all_quotes_tfidf)
 
             # Get indices of top similar quotes
             top_indices = similarity_matrix.argsort(axis=1)[:, -num_recommendations:]
 
-            # Get top recommended quotes
-            recommended_quotes_ids = [all_quotes_texts[i.item()] for i in top_indices.ravel()]
-            recommended_quotes = Quote.objects.filter(text__in=recommended_quotes_ids)            
-        else:
-        # # If the favorite quotes = 0
-        # if not liked_quotes_texts:
-            # If the user has not liked any quotes, generate recommendations from top 100 most liked quotes
-            liked_quotes_texts = Quote.objects.annotate(num_likes=Count('quoteslikes')).order_by('-num_likes', 'id')[:10]
-            recommended_quotes_list = [quote.text for quote in liked_quotes_texts]
-            recommended_quotes = Quote.objects.filter(text__in=recommended_quotes_list)            
-            # print(recommended_quotes)
-            return recommended_quotes
+            # **Fix: Convert numpy int64 to Python integers**
+            recommended_quotes_texts = [all_quotes_texts[int(i)] for i in top_indices.flatten()]
+            recommended_quotes = Quote.objects.filter(text__in=recommended_quotes_texts)
+            # print("the user has liked before,")            
+            # print(type(recommended_quotes))
+            
+
         
+        else:
+            # If the user has not liked any quotes, generate recommendations from top 100 most liked quotes
+            # top_quotes = Quote.objects.annotate(num_likes=Count('quoteslikes')).order_by('-num_likes')[:100]
+            # paginator = Paginator(top_quotes, num_recommendations)
+            # page = 1  # Assuming this is the default page
+            # recommended_quotes = paginator.get_page(page)
+            
+            recommended_quotes = Quote.objects.annotate(num_likes=Count('quoteslikes')).order_by('-num_likes')[:100]
+            
+            # print("the user has not liked any quotes,")
+            # print(type(recommended_quotes))
         
         end_time = time.time()  # Record end time
         execution_time = end_time - start_time
         print(f"Execution time: {execution_time:.4f} seconds")  # Display execution time
 
         return recommended_quotes
-    
