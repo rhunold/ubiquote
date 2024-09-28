65a6277146bec0e28e6c39ae1ac6f4fc9f702c25
Hunold <raphael.hunold@gmail.com>
2024-09-28 13:38:45 +0200

    small fix

--- ubiquote/texts/urls.py
+++ ubiquote/texts/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
 from . import views
-from .views import home, HomeView
-from .views import GetCategoriesView, GetCategoryView
 
 app_name = 'texts'
 
 urlpatterns = [
     # path('', views.home, name='home'),
-    path('', HomeView.as_view(), name='home'),    
+    path('', views.HomeView.as_view(), name='get-home'),    
     
-    path('categories/', GetCategoriesView.as_view(), name='get-categories'),
+    path('categories/', views.GetCategoriesView.as_view(), name='get-categories'),
     # path('category/<int:pk>/', GetCategoryView.as_view(), name='get-category'),
-    path('category/<str:slug>/', GetCategoryView.as_view(), name='get-category'),
+    path('category/<str:slug>/', views.GetCategoryView.as_view(), name='get-category'),
 ]
